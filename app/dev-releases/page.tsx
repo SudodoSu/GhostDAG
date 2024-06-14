58275a40965ca0c2ddc7b708961334e7453e49bc
SudoDosu <93739034+SudodoSu@users.noreply.github.com>
2024-06-14 22:23:10 +0200

    style

--- app/dev-releases/page.tsx
+++ app/dev-releases/page.tsx
@@ -1,5 +1,46 @@
+import PageDirection from "@/components/PageDestination/PageDirection";
+import { DevReleasesSliderData } from "@/lib/DevReleasesSliderData";
+import Image from "next/image";
+import Link from "next/link";
+
 function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <PageDirection
+        image="/images/PageDirection/dev-news.png"
+        title="Dev Releases"
+        PagePath="Daily Dev Releases"
+      />
+      <div className="container2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[calc(var(--one)*50)]">
+        {DevReleasesSliderData.data.slice(1).map((item, index) => (
+          <div key={index} className="">
+            <div className="dev_releases_card ">
+              <Link
+                href={item.href}
+                key={index}
+                className="dev_releases_image_container "
+              >
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  width={500}
+                  height={282}
+                  className="w-full h-full"
+                />
+                <h3 className="dev_releases_image_title">{item.title}</h3>
+              </Link>
+              <h6 className="">{item.title}</h6>
+              <p className="slider_description">{item.des}</p>
+              <span>{item.date}</span>
+              <Link href={item.href} className="dev_releases_read_more">
+                Read More
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default page;
